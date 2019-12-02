b1290463360aefa9a0ae6cb8abac2fd7ab38ba22
xinput <yuan_lai1234@163.com>
2019-12-02 14:42:42 +0800

    spring 基础环境搭建

---
+++ spring-mvc-base/src/main/java/com/spring/mvc/MvcApp.java
@@ -0,0 +1,8 @@
+package com.spring.mvc;
+
+/**
+ * @author <a href="mailto:xinput.xx@gmail.com">xinput</a>
+ * @Date: 2019-12-02 11:45
+ */
+public class MvcApp {
+}
---
+++ spring-mvc-base/src/main/java/com/spring/mvc/mapper/UserInfoMapper.java
@@ -0,0 +1,32 @@
+package com.spring.mvc.mapper;
+
+import com.spring.mvc.entity.UserInfo;
+import com.spring.mvc.entity.UserInfoExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UserInfoMapper {
+
+    int countByExample(UserInfoExample example);
+
+    int deleteByExample(UserInfoExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    List<UserInfo> selectByExample(UserInfoExample example);
+
+    UserInfo selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") UserInfo record, @Param("example") UserInfoExample example);
+
+    int updateByExample(@Param("record") UserInfo record, @Param("example") UserInfoExample example);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
